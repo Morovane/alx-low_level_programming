8050759a705bd42b3b54b941982a95a5c2f96da9
morovane <elfilalimarouane.1997@gmail.com>
2023-03-16 13:14:20 -0700

    Write a C program that prints the size of various types on the computer it is compiled and run on.
    You should produce the exact same output as in the example
    Warnings are allowed
    Your program should return 0
    You might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %d byte(s)", sizeof(c));
-	printf("Size of a int: %d byte(s)"), sizeof(a);
-	printf("Size of a long int: %d byte(s)"), sizeof(b);
-	printf("Size of a long long int: %d byte(s)", sizeof(d);
-	printf("Size of a float: %d byte(s)", sizeof(f);
+	printf("Size of a char: %lu byte(s)", sizeof(c));
+	printf("Size of a int: %lu byte(s)"), sizeof(a);
+	printf("Size of a long int: %lu byte(s)"), sizeof(b);
+	printf("Size of a long long int: %lu byte(s)", sizeof(d);
+	printf("Size of a float: %lu byte(s)", sizeof(f);
 
 	return (0);
 }
