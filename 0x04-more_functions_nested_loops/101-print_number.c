95bdf17149dce91ec37a0515ed686cf6b6d6f1bf
Marouane <44686783+morovane@users.noreply.github.com>
2023-03-24 21:38:47 +0000

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,38 +8,17 @@
  */
 void print_number(int n)
 {
-	int d = 1, test = 1;
+	int d = n;
 
-	if (n < 0)
+	if (d < 0)
 	{
-		n = n * -1;
+		d = d * -1;
 		_putchar('-');
 	}
 
-	while (test)
+	if ((d / 10) > 0)
 	{
-		if (n / (d * 10) > 0)
-		{
-			d = d * 10;
-		}
-		else
-		{
-			test = 0;
-		}
+		print_number(d / 10);
 	}
-
-
-	while (n >= 0)
-	{
-		if (d == 1)
-		{
-			_putchar(n % 10 + '0');
-			break;
-		}
-		else
-		{
-			_putchar((n / d % 10) + '0');
-			d = d / 10;
-		}
-	}
+	_putchar((d % 10) + '0');
 }
