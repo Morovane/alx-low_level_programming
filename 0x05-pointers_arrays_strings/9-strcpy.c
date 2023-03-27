3615800019d356921d8c5aa119acbe52e114d27f
morovane <elfilalimarouane.1997@gmail.com>
2023-03-27 15:33:12 -0700

    Write a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
    Return value: the pointer to dest
    FYI: The standard library provides a similar function: strcpy. Run man strcpy to learn more.

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - Write a function that copies the string pointed
+ * @dest: parametre 1
+ * @src: parametre 2
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
