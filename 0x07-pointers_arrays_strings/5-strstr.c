3588eeaa339629154aa94a88745995cdc638eabc
morovane <elfilalimarouane.1997@gmail.com>
2023-04-03 15:40:55 -0700

    Write a function that locates a substring.
    Prototype: char *_strstr(char *haystack, char *needle);
    The _strstr() function finds the first occurrence of the substring needle in the string haystack. The terminating null bytes (\0) are not compared
    Returns a pointer to the beginning of the located substring, or NULL if the substring is not found.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - that locates a substring
+ * @haystack: array
+ * @needle: parametre
+ * Return: hastack or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index, i = 0;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (haystack[i] > '\0')
+	{
+		index = 0;
+
+		if (haystack[i] == needle[index])
+		{
+			do {
+				if (needle[index] == '\0')
+					return (haystack + i);
+				index++;
+			} while (haystack[i + index] == needle[index]);
+
+		}
+		i++;
+	}
+	return ('\0');
+}
