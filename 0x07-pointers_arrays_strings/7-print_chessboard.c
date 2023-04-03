cecbf3a771d27a8ecec3bacb0c61f5942d877cb6
morovane <elfilalimarouane.1997@gmail.com>
2023-04-03 16:02:45 -0700

    Write a function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** print_chessboard - that print chess board
+ * @a: array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, j;
+
+	for (row = 0; a[row][7] > '\0'; row++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[row][j]);
+		}
+		_putchar('\n');
+	}
+
+}
