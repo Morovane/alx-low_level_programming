c11d42db347f3e96a5ef1433ecd186c97f4e4800
morovane <elfilalimarouane.1997@gmail.com>
2023-04-03 13:20:07 -0700

    Write a function that copies memory area.
    Prototype: char *_memcpy(char *dest, char *src, unsigned int n);
    The _memcpy() function copies n bytes from memory area src to memory area dest
    Returns a pointer to dest

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,16 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index ;
+	unsigned int index;
 
 	for (index = 0; index < n; index++)
 	{
 		dest[index] = src[index];
-		/*or
+		/**
+		 * or
 		 * *dest = src[index];
-		 *dest++;*/
+		 * dest++;
+		 */
 	}
 	return (dest);
 }
