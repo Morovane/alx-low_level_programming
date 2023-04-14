a0cdc0b1bbafdb815be93a18967abc08ecb961e6
Morovane <elfilalimarouane.1997@gmail.com>
2023-04-14 14:32:35 -0700

    /

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,7 +33,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	calloc = malloc(sizeof(size) * nmemb);
+	calloc =(void *) malloc(sizeof(size) * nmemb);
 
 	if (calloc == NULL)
 		return (NULL);
