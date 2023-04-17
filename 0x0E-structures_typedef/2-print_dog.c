97367ac3f20cd474b77da1070a2fb2351fcbb589
Morovane <elfilalimarouane.1997@gmail.com>
2023-04-17 15:47:49 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			d->name = "(nil)";
+		else if (d->age == '\0')
+			d->age = 0;
+		else if (d->owner == NULL)
+			d->owner = "(nil)";
+
+		else
+			printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+	else
+		printf("nothing");
+}
