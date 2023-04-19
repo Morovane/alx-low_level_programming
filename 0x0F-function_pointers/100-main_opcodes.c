2c65291c23c5e902dca08d6198c16cfadcd14030
Morovane <elfilalimarouane.1997@gmail.com>
2023-04-19 16:26:49 -0700

    opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int byte, i;
+	int (*adress)(int ,char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	byte = atoi(argv[1]);
+	if (byte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < byte; i++)
+	{
+		opcode = *(unsigned char *)adress;
+		printf("%.2x", opcode);
+
+		if (i != byte - 1)
+			printf(" ");
+
+		adress++;
+	}
+	printf("\n");
+	return (0);
+
+}
