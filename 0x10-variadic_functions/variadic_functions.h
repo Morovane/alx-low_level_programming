6382f193bd8c9fb67bc7165f5cfa096412c24d0a
Morovane <elfilalimarouane.1997@gmail.com>
2023-04-20 13:23:43 -0700

    update header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+typedef struct printer {
+
+	char *symb;
+	void (*print)(va_list arg);
+} prt;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
