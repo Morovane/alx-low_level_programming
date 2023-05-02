521565b367a384b7ea5f9ee0ffb322d36e778d66
Morovane <elfilalimarouane.1997@gmail.com>
2023-05-02 15:16:57 -0700

    Write a function that adds a new node at the beginning of a list_t list.
    Prototype: list_t *add_node(list_t **head, const char *str);
    Return: the address of the new element, or NULL if it failed
    str needs to be duplicated
    You are allowed to use strdup

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 
 /**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: A pointer to the head of the list_t list
+ * @str: the string to be added to the list list_t
+ * Return: If the function fails - NULL.
+ *	Otherwise - the address of the new element.
  */
 list_t *add_node(list_t **head, const char *str)
 {
